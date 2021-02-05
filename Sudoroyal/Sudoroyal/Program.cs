7d5276597b6640d94c3f261782d906a67aae6f28
brlimarce <blarce@up.edu.ph>
2021-02-05 17:39:47 +0800

    Created C# WinForms app

---
+++ Sudoroyal/Sudoroyal/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using Sudoroyal.Views.Screen;
+
+namespace Sudoroyal
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Splash()); // To run the Splash screen.
+            Application.Run(new Title());
+        }
+    }
+}
