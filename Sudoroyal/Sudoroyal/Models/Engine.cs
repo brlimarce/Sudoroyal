e9b789c4058e6299f5c8dc73e13b087fe0b509bc
brlimarce <blarce@up.edu.ph>
2021-02-05 17:46:28 +0800

    Added C# model for Python integration

---
+++ Sudoroyal/Sudoroyal/Models/Engine.cs
@@ -0,0 +1,33 @@
+﻿using IronPython.Hosting;
+
+namespace Sudoroyal.Models
+{
+    /* -- Engine
+     * To get the class from the Python files. */
+    public class Engine
+    {
+        /* -- Methods
+         * To get the executable Python file. */
+        private dynamic getExecutable(string fileName)
+        {
+            var engine = Python.CreateEngine(); // To create the engine.
+            dynamic executable = engine.ExecuteFile(fileName); // To get the executable file.
+            return executable;
+        }
+
+        // To get the Board class.
+        public dynamic getBoardClass()
+        {
+            dynamic executable = getExecutable("board.py");
+            return executable.Board();
+        }
+
+        // To get the Files class.
+        public dynamic getFileClass()
+        {
+            // To get the class.
+            dynamic executable = getExecutable("files.py");
+            return executable.Files();
+        }
+    }
+}
